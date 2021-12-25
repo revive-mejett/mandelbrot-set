270d5f190713c656af5470d0ad5a8333fe27306b
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-12-24 21:39:38 -0500

    pacman drawing

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 
 document.addEventListener('DOMContentLoaded', setup)
 
+let x = 0
+let debounce = false
+let toggleClicked = false
 
 function setup() {
     const canvas = document.createElement('canvas')
@@ -14,15 +17,46 @@ function setup() {
 
     document.querySelector('body').appendChild(canvas)
     let ctx = canvas.getContext("2d")
+    ctx.fillStyle = "#FF0000";
     ctx.moveTo(100,100)
     ctx.ellipse(100,100,50,50,Math.PI/4,0,3*Math.PI/2,false)
     ctx.lineTo(100,100)
     ctx.stroke()
+    document.addEventListener('mousemove', logpos)
+    
     
     
 
     console.log('sova');
     document.querySelector('body').appendChild(canvas)
+    document.addEventListener('mousedown', toggleClick)
 
+    
 
+}
+
+function logpos(evt) {
+    if (x <= 5 && !debounce && toggleClicked) {
+        debounce = true
+        console.log(evt.screenX)
+        setTimeout(() => {
+            debounce = false
+            drawCircle(evt.screenX, evt.screenY)
+        }, 20);
+    }
+
+}
+
+function drawCircle(x, y) {
+    const canvas = document.querySelector('.mandelbrort')
+    let ctx = canvas.getContext("2d")
+    ctx.moveTo(x,y)
+    ctx.ellipse(x,y,50,50,Math.PI/4,0,3*Math.PI/2,false)
+    ctx.lineTo(x,y)
+    ctx.stroke()
+    document.addEventListener('mousemove', logpos)
+}
+
+function toggleClick() {
+    toggleClicked = !toggleClicked;
 }
