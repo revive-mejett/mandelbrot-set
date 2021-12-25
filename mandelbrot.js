157c76d154f7052cbaa23e5d8aa9aecf22bd433d
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-12-25 00:08:50 -0500

    got a way to fill a whole canvas!

--- mandelbrot.js
+++ mandelbrot.js
@@ -10,7 +10,7 @@ let toggleClicked = false
 let colourHue = 0;
 
 //global
-const imageWidth = 10
+const imageWidth = 2
 
 function setup() {
     const canvas = document.createElement('canvas')
@@ -33,21 +33,41 @@ function drawFullImage() {
     const canvas = document.querySelector('.mandelbrort')
     console.log(canvas.getAttribute('width'))
     console.log(canvas.getAttribute('height'))
+    console.log(typeof parseNumber(canvas.getAttribute('height')))
+
 
     let ctx = canvas.getContext('2d')
-    let pixelImage = ctx.createImageData(imageWidth, imageWidth)
-    console.log(pixelImage.data.length)
+    let pixelImage
+    let column
+    let row
+    // console.log(pixelImage.data.length)
     
-    //color all the pixels
-    for (let i = 0; i < pixelImage.data.length; i +=4) {
-
-        pixelImage.data[i] = 255; //r
-        pixelImage.data[i+1] = 0; //g
-        pixelImage.data[i+2] = 0; //b
-        pixelImage.data[i+3] = 255; //a
+    for (row = 0; row < parseNumber(canvas.getAttribute('height')); row+=imageWidth) {
+        //color a full row
+        for (column = 0; column < parseNumber(canvas.getAttribute('width')); column+=imageWidth) {
+            //color the single pixel (or the square if each unit > 1 pixel)
+            colourSquare()
+            ctx.putImageData(pixelImage,column,row)
+        }
     }
     
-    // let pixelImage = 
-    ctx.putImageData(pixelImage,imageWidth,imageWidth)
+    
+    
+    
+    
 
+    function colourSquare() {
+        pixelImage = ctx.createImageData(imageWidth, imageWidth)
+        for (let i = 0; i < pixelImage.data.length; i += 4) {
+            pixelImage.data[i] = column //r
+            pixelImage.data[i + 1] = 0 //g
+            pixelImage.data[i + 2] = row //b
+            pixelImage.data[i + 3] = 255
+        }
+    }
+}
+
+
+function parseNumber(string) {
+    return Number(string.replace('px', ''))
 }
