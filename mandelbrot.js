6f65e1b9d8dc4c96ddb14ba18c38fcc15f0871c3
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-12-26 00:49:22 -0500

    Revert "alternative way to generate set, changed algorithm that sets a start x and end x, same for i"
    This reverts commit 9d38144868cb81fe909d12d0a3372e4b07fb289d.

--- mandelbrot.js
+++ mandelbrot.js
@@ -27,21 +27,14 @@ let zoom = 100
 let panX = 0
 let panI = 0
 
-//startXRange
-let startX = -2
-let startI = 2
-let endX = 2
-let endI = -2
-let currentX = -2
-let currentI = 2
 function setup() {
     const canvas = document.createElement('canvas')
 
     canvas.setAttribute('class', 'mandelbrot')
     canvas.setAttribute('height', `${canvasHeight}px`)
     canvas.setAttribute('width', `${canvasWidth}px`)
-    coordinateOffsetX = -parseNumber(canvas.getAttribute('width'))/2
-    coordinateOffsetI = -parseNumber(canvas.getAttribute('height'))/2
+    coordinateOffsetX = parseNumber(canvas.getAttribute('width'))/2
+    coordinateOffsetI = parseNumber(canvas.getAttribute('height'))/2
 
     document.querySelector('#canvas-section').appendChild(canvas)
     console.log('phoenix loves jett')
@@ -57,20 +50,13 @@ function setup() {
 
 
     drawFullImage()
-    canvas.addEventListener('dblclick', zoomIn)
-}
+    
 
-function zoomIn(e) {
-    const canvas = document.querySelector('.mandelbrot')
-    let ctx = canvas.getContext('2d')
-    ctx.clearRect(0, 0, canvas.width, canvas.height) //remove the previous content of the canvas
-    setTimeout(() => {
-        drawFullImage()
-    }, 200);
 }
 
 function reRender(e) {
     e.preventDefault()
+    console.log('sova and phoenix');
     const canvas = document.querySelector('.mandelbrot')
     let ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, canvas.width, canvas.height) //remove the previous content of the canvas
@@ -132,18 +118,17 @@ function drawFullImage() {
     
     //colour all the rows in a loop
     for (row = 0; row < parseNumber(canvas.getAttribute('height')); row+=imageWidth) {
-        currentX = startX
         colourRow()
-        currentI-= Math.abs(endI-startI)/canvasHeight
-        
     }
+    
+    
+    
     
     //colour a full row.
     function colourRow() {
         for (column = 0; column < parseNumber(canvas.getAttribute('width')); column += imageWidth) {
             //color the single pixel (or the square if each unit > 1 pixel)
             colourSquare()
-            currentX+= Math.abs(endX-startX)/canvasWidth
             ctx.putImageData(pixelImage, column, row)
         }
     }
@@ -152,10 +137,7 @@ function drawFullImage() {
     function colourSquare() {
         pixelImage = ctx.createImageData(imageWidth, imageWidth)
         // get a coordinate based on the position of this square
-        //might need to change
-        // let currentCoordinate = new Coordinate(convertToXCoordinate(column), convertToICoordinate(row))
-        console.log(currentX + ', ' + currentI)
-        let currentCoordinate = new Coordinate(currentX, currentI)
+        let currentCoordinate = new Coordinate(convertToXCoordinate(column), convertToICoordinate(row))
         let colour = determineColour(determineIterations(currentCoordinate))
 
         for (let i = 0; i < pixelImage.data.length; i += 4) {
@@ -164,7 +146,6 @@ function drawFullImage() {
             pixelImage.data[i + 2] = colour.blue //b
             pixelImage.data[i + 3] = 255 //aplha
         }
-        
     }
 }
 
@@ -217,7 +198,7 @@ function parseNumber(string) {
  * @returns {number}
  */
 function convertToXCoordinate(xCanvasPos) {
-    let converted = ((xCanvasPos + coordinateOffsetX + panX) / zoom)
+    let converted = ((xCanvasPos - coordinateOffsetX) / zoom)  + panX*2/zoom
     return converted
 }
 
@@ -227,7 +208,7 @@ function convertToXCoordinate(xCanvasPos) {
  * @returns {number}
  */
 function convertToICoordinate(yCanvasPos) {
-    let converted = ((yCanvasPos + coordinateOffsetI + panI) / zoom)
+    let converted = ((yCanvasPos - coordinateOffsetI) / zoom) + panI*2/zoom
     return converted
 }
 
