a115ad9b004fd1b6e91c31435c4650751b35f6b6
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-12-29 14:44:40 -0500

    fixed cycling range issue

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,17 +1,16 @@
-
 'use strict'
-
 import Coordinate from './src/Coordinate.js'
+
+// the width of a unit square.
+const imageWidth = 1
+const maxIterations = 2000
+const canvasWidth = 400
+const canvasHeight = 400
+const cycleMultiplier = 4 //determines the increment of new cycles of colours
 
 document.addEventListener('DOMContentLoaded', setup)
 
 
-
-// the width of a unit square.
-const imageWidth = 1
-const maxIterations = 400
-const canvasWidth = 400
-const canvasHeight = 400
 
 let coordinateOffsetX
 let coordinateOffsetI
@@ -46,27 +45,24 @@ function setup() {
  
     document.addEventListener('keydown', (e) => {
             if (e.key === 'ArrowRight') {
-                console.log('sova')
                 panX += panXmultiplier / magnification
                 reRenderKey()
             }
             if (e.key === 'ArrowLeft') {
-                console.log('killjoy')
                 panX -= panXmultiplier / magnification
                 reRenderKey()
             }
             if (e.key === 'ArrowUp') {
-                console.log('raze')
                 panI -= panImultiplier / magnification
                 reRenderKey()
             }
             if (e.key === 'ArrowDown') {
-                console.log('omen')
                 panI += panImultiplier / magnification
                 reRenderKey()
             }
         })
     drawFullImage()
+    console.log(determineColour(450))
     
 
 }
@@ -85,7 +81,6 @@ function reRenderKey() {
 
 function reRender(e) {
     e.preventDefault()
-    console.log('sova and phoenix');
     const canvas = document.querySelector('.mandelbrot')
     let zoomInputValue = document.querySelector('#zoom').value
 
@@ -108,8 +103,6 @@ function updatePanMultiplierValues() {
     const panImultiplierInput = document.querySelector('#pan-i').value
     document.querySelector('#pan-x-display').textContent = panXmultiplierInput
     document.querySelector('#pan-i-display').textContent = panImultiplierInput
-    console.log(panXmultiplierInput)
-    console.log(panImultiplierInput)
     panXmultiplier = panXmultiplierInput
     panImultiplier = panImultiplierInput
 
@@ -178,6 +171,8 @@ function drawFullImage() {
         pixelImage = ctx.createImageData(imageWidth, imageWidth)
         // get a coordinate based on the position of this square
         let currentCoordinate = new Coordinate(convertToXCoordinate(column), convertToICoordinate(row))
+
+        //determine the colour based on the number of iterations
         let colour = determineColour(determineIterations(currentCoordinate))
 
         for (let i = 0; i < pixelImage.data.length; i += 4) {
@@ -194,97 +189,114 @@ function drawFullImage() {
  * @param {int} numberIterations -- The number of iterations taken
  * @returns {object} -- JSON object with rgb values
  */
+
+let logged = false
 function determineColour(numberIterations) {
 
-    let iterationLowerBound;
-    let iterationRangeSize;
 
     const navyToBlueBound = 30;
-    const cyanBound = 60;
-    const greenBound = 100;
-    const yellowBound = 200;
-    const redBound = 300;
-    const blueBound = 400;
-    let black = {
+    const cyanBoundPercentage = 0.2;
+    const greenBoundPercentage = 0.4;
+    const yellowBoundPercentage = 0.6;
+    const redBoundPercentage = 0.8;
+
+    
+    //for more than 30 iterations, colour them in a rainbow cycle that repeats until max iterations
+
+
+    let prevCycleLowerBound
+    let cycleLowerBound = 1
+    let cycleRangeSize
+    //rainbow cycle repeats after each 10^cycle iterations
+    for (let upperCycleBound = 100; upperCycleBound <= maxIterations; upperCycleBound*=cycleMultiplier) {
+        
+        let bracketRangeSize
+        cycleRangeSize = upperCycleBound-cycleLowerBound //300
+        // console.log('cycle lower: ' +cycleLowerBound)
+        // console.log('cycle upper b: ' +upperCycleBound)
+        // console.log(cycleRangeSize)
+        // console.log('#iterations: ' +numberIterations)
+        // console.log('cyan iterations ' + (cyanBoundPercentage*cycleRangeSize + cycleLowerBound))
+        // console.log('green iterations ' + (greenBoundPercentage*cycleRangeSize + cycleLowerBound))
+        // console.log('yellow iterations ' + (yellowBoundPercentage*cycleRangeSize + cycleLowerBound))
+        // console.log('red iterations ' + (redBoundPercentage*cycleRangeSize + cycleLowerBound))
+        // console.log('blue iterations ' + (upperCycleBound))
+    
+        
+        //blue to cyan 1-20
+        if (numberIterations >= cycleLowerBound && numberIterations < cyanBoundPercentage*cycleRangeSize + cycleLowerBound) {
+            bracketRangeSize = cyanBoundPercentage*cycleRangeSize //60
+            
+
+            return {
+                red : 0,
+                green : (numberIterations - cycleLowerBound)/bracketRangeSize * 255, // more green
+                blue : 255
+            }
+        }
+
+            //cyan to green 21-40
+        else if (numberIterations >= cyanBoundPercentage*cycleRangeSize + cycleLowerBound && numberIterations < greenBoundPercentage*cycleRangeSize + cycleLowerBound) {
+
+            bracketRangeSize = greenBoundPercentage*cycleRangeSize - cyanBoundPercentage*cycleRangeSize
+            return {
+                red : 0,
+                green : 255,
+                blue : (bracketRangeSize - (numberIterations - cyanBoundPercentage*cycleRangeSize))/bracketRangeSize * 255 // less blue
+            }
+        }
+
+        //green to yellow 41-60
+        else if (numberIterations >= greenBoundPercentage*cycleRangeSize + cycleLowerBound && numberIterations < yellowBoundPercentage*cycleRangeSize + cycleLowerBound) {
+
+
+            bracketRangeSize = yellowBoundPercentage*cycleRangeSize - greenBoundPercentage*cycleRangeSize
+            return {
+                red : (numberIterations - greenBoundPercentage*cycleRangeSize)/bracketRangeSize * 255, // more red
+                green : 255,
+                blue : 0
+            }
+        }
+
+            //yellow to red 61-80
+        else if (numberIterations >= yellowBoundPercentage*cycleRangeSize + cycleLowerBound && numberIterations < redBoundPercentage*cycleRangeSize + cycleLowerBound) {
+
+
+            bracketRangeSize = redBoundPercentage*cycleRangeSize - yellowBoundPercentage*cycleRangeSize
+
+            return {
+                red : 255,
+                green : (bracketRangeSize - (numberIterations - yellowBoundPercentage*cycleRangeSize))/bracketRangeSize * 255, //less green
+                blue : 0
+            }
+        }
+
+        //red to blue 81-100
+        else if (numberIterations >= redBoundPercentage*cycleRangeSize + cycleLowerBound && numberIterations <= upperCycleBound) {
+
+
+            bracketRangeSize = upperCycleBound - redBoundPercentage*cycleRangeSize
+
+            return {
+                red : (bracketRangeSize - (numberIterations - redBoundPercentage*cycleRangeSize))/bracketRangeSize * 255, //less red,
+                green : 0,
+                blue : (numberIterations - redBoundPercentage*cycleRangeSize)/bracketRangeSize * 255 //more blue
+            }
+        }
+
+        else {
+            cycleLowerBound = upperCycleBound //move to next cycle
+        }
+    }
+
+    //return black if over max iterations
+    return {
         red : 0,
         green : 0,
         blue : 0
     }
 
-    //YANDERE DEV CODE DETECTED!!!!
-    if (numberIterations === maxIterations) {
-        return black
-        
-    } else if (numberIterations >= (maxIterations - 100)) {
-
-        iterationLowerBound = redBound //300
-        iterationRangeSize = blueBound - redBound //100
-        //from yellow to red (300 to 400 iterations)
-        return {
-            red : (iterationRangeSize - (numberIterations - iterationLowerBound))/iterationRangeSize * 255, //less blue,
-            green : 0,
-            blue : (numberIterations - iterationLowerBound)/iterationRangeSize * 255 //more blue
-        }
-    } else if (numberIterations >= (maxIterations - 200)) {
-
-        iterationLowerBound = yellowBound //200
-        iterationRangeSize = redBound - yellowBound //100
-        //from yellow to red (200 to 300 iterations)
-        return {
-            red : 255,
-            green : (iterationRangeSize - (numberIterations - iterationLowerBound))/iterationRangeSize * 255,
-            blue : 0
-        }
-    } else if (numberIterations >= (maxIterations - 300)) {
-
-        iterationLowerBound = greenBound //100
-        iterationRangeSize = yellowBound - greenBound //100
-        //from green to yellow (101 to 200 iterations)
-        return {
-            //red : numberIterations/(maxIterations - 330) * 255,
-            red : (numberIterations - iterationLowerBound)/iterationRangeSize * 255,
-            green : 255,
-            blue : 0
-        }
-    } else if (numberIterations >= (maxIterations - 340)) {
-
-        iterationLowerBound = cyanBound //60
-        iterationRangeSize = greenBound - cyanBound // 40
-        //from cyan to green (61 to 100 iterations)
-        return {
-            //red : numberIterations/(maxIterations - 330) * 255,
-            red : 0,
-            green : 255,
-            blue : (iterationRangeSize - (numberIterations - iterationLowerBound))/iterationRangeSize * 255
-        }
-    } else if (numberIterations >= (maxIterations - 370)) {
-
-        //from blue to cyan (31 to 60 iterations)
-        iterationLowerBound = navyToBlueBound
-        iterationRangeSize = maxIterations - (maxIterations - iterationLowerBound)
-        return {
-            red : 0,
-            green : (numberIterations - iterationLowerBound)/iterationRangeSize * 255,
-            blue : 255
-        }
-    } else {
-        //from navy blue to blue (0 to 30 max iterations)
-        iterationLowerBound = navyToBlueBound;
-        iterationRangeSize = maxIterations - (maxIterations - iterationLowerBound)
-        return {
-            red : 0,
-            green : 0,
-            blue : numberIterations/iterationRangeSize * 255
-        }
-    }
-
-
 }
-
-function parseNumber(string) {
-    return Number(string.replace('px', ''))
-}
-
 /**
  * Converts to x coordinate in mandelbrot set
  * @param {number} xCanvasPos 
